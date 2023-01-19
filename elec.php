bb6865052edd235a5f304bd2bfc884dac87287f1
Julian-root <adjibiadechinan@gmail.com>
2023-01-19 16:18:31 +0100

    test

---
+++ elec.php
@@ -0,0 +1,175 @@
+<?php
+/*
+Template Name: Elec
+*/
+?>
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+
+    <title>HCORI - Solution hydroélectrique pour la production d'énergie renouvelable</title>
+    <link rel="shortcut icon" href="https://hcori.com/files/images/favicon.ico">
+    
+    <link href="https://hcori.com/files/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+
+    <link href="https://hcori.com/files/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://hcori.com/files/css/all.min.css">
+
+    <link rel="stylesheet" href="https://hcori.com/files/design.css">
+    //kjhjhj
+    
+</head>
+
+<body>
+    <header class="header">
+        <nav class="navbar navbar-default navbar-fixed-top" style="background-color: #000033 !important; border-bottom: 2px solid red;">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg">
+                    <a class="navbar-brand" href="#">HCORI</a>
+                    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+                    </button>
+                    <div class="navbar-collapse collapse" id="navbarCollapse" style="">
+                        <ul class="navbar-nav ml-auto" >
+                            <li><a href="#accueil" >Accueil</a></li>
+                            <li><a href="#technologie">Technologie</a></li>
+                            <li><a href="#solution">Solution</a></li>
+                            <li><a href="#application">Application</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </nav>
+    </header>
+
+    <section class="home-section" id="accueil">
+        <div class="home-section-overlay"></div>
+		<div class="container">
+	        <div class="row">
+	            <div class="col-md-12 text-center" style="position: relative;">
+	          		<h2 style="color: #0606fc;">Ici juste là</h1>
+	          		<h2 style="color: #0606fc;">Nous produisons de l'électricité décarbonnée</h2>
+	          		<h2 style="color: #0606fc;" class="hero-text">Vous ne voyez rien ?</h2>
+	          		<h2 style="color: #0606fc;" class="hero-text">Vous n'entendez rien ?</h2>
+	          		<h2 style="color: #0606fc;" class="hero-text">C'est normal.</h2>
+                    <!-- <a href="#technologie" class="arrow-down scrool"><i class="pe-7s-angle-down-circle"></i></a> -->
+	            </div>
+	        </div>
+		</div>
+    </section>
+
+    <section id="technologie" class="section-test section-bottom-border" style="z-index:2;">
+        <div class="container">
+	        <div class="row">
+	            <div class="col-md-6 padding-top-120">
+                    <h4 class="hero-text" style="color:#000033;">1ère solution hydroélectrique pour la <br> production d'énergie renouvelable <br> par gravité en mer et en lac
+                    </h4>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+	            </div>
+                <div class="col-md-1"></div>
+	            <div class="col-md-5">
+                    <!-- <br> -->
+                    <img src="https://hcori.com/files/square-map.jpg" alt="" id="carte" 
+                    style="border-radius: 50%;overflow: hidden; border: 15px solid white; 
+                    width: 110%; height: 110%;"
+                    >
+                </div>
+	        </div>
+		</div>
+    </section>
+
+    <section id="solution" class="section-two section-bottom-border" style="z-index:2;">
+        <div class="container">
+	        <div class="row">
+	            <div class="col-md-6 padding-top-120">
+                    <h4 class="hero-text" style="color:#000033;">1ère solution hydroélectrique pour la <br> production d'énergie renouvelable <br> par gravité en mer et en lac
+                    </h4>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+	            </div>
+                <div class="col-md-1"></div>
+	            <div class="col-md-5">
+                    <!-- <br> -->
+                    <img src="https://hcori.com/files/square-map.jpg" alt="" id="carte"
+                    style="border-radius: 50%;overflow: hidden;  
+                    width: 110%; height: 110%;"
+                    >
+                </div>
+	        </div>
+		</div>
+    </section>
+
+    <div class="footer" id="application">
+        <div class="col-md-12 text-center" style="position: relative;">
+            <h2>Une technologie 4 applications</h2>
+            <h6>Une combinaison unique de fonctionnalités</h6>
+            <br>
+        </div>
+        <div class="container footer-top">
+            <div class="row">
+                <div class="col-md-3 text-center">
+                    <img src="https://hcori.com/files/light.jpeg" alt="" width="75px" height="75px" style="border-radius: 50%;">
+                    <h5>Electricité</h5>
+                </div>
+                <div class="col-md-3 text-center">
+                    <img src="https://hcori.com/files/h2.jpeg" alt="" width="75px" height="75px" style="border-radius: 50%;">
+                    <h5>Hydrogéné</h5>
+                </div>
+                <div class="col-md-3 text-center">
+                    <img src="https://hcori.com/files/water.jpeg" alt="" width="75px" height="75px" style="border-radius: 50%;">
+                    <h5>Water</h5>
+                </div>
+                <div class="col-md-3 text-center">
+                    <img src="https://hcori.com/files/icons.jpeg" alt="" width="75px" height="75px" style="border-radius: 50%;">
+                    <h5>Data</h5>
+                </div>
+            </div>
+        </div>
+                
+        <div class="container-fluid footer-bottom px-0" style="background-color: #000033; border-top: 2px solid red;">
+            <div class="row no-gutters mx-0">
+                <div class="col-md-6 text-center">
+                </div>
+                <div class="col-md-6 text-center">
+                    <ul class="footer_social">
+
+                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+
+                        <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
+
+                        <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+
+                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+
+                        <li><a href="#"><i class="fab fa-skype"></i></a></li>
+
+                        <li><a href="#"><i class="fab fa-dribble"></i></a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+                
+    </div>
+
+
+    <script src="https://hcori.com/files/js/jquery-3.3.1.min.js"></script>
+    <script src="https://hcori.com/files/js/custom.js"></script>
+
+</body>
+</html>
